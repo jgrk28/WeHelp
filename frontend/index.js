89c05229283f420a945344380e5326e3eef114d3
jgrk28 <jacobkaplan28@gmail.com>
2022-11-21 16:15:47 +0300

    Add sign up form

--- frontend/index.js
+++ frontend/index.js
@@ -1,41 +1,66 @@
 
-function openForm() {
-	document.getElementById('signinFormPopup').style.display = 'block';
-	document.getElementById("blur").style.display = "block";
+function openForm(formId) {
+  document.getElementById(formId).style.display = 'block';
+  document.getElementById("blur").style.display = "block";
 }
 
-function closeForm() {
-  document.getElementById("signinFormPopup").style.display = "none";
+function closeForm(formId) {
+  document.getElementById(formId).style.display = "none";
   document.getElementById("blur").style.display = "none";
 }
 
-const signinForm = document.getElementById("signinForm");
+const loginForm = document.getElementById("loginForm");
 
-signinForm.addEventListener("submit", (e) => {
+loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (validateSigninInput()) signIn();
+  if (validateLoginInput()) login();
 });
 
-function validateSigninInput() {
-  let email = document.getElementById("email").value;
-  let psw = document.getElementById("psw").value;
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (validateSignupInput()) signup();
+});
+
+function validateLoginInput() {
+  let email = document.getElementById("login-email").value;
+  let psw = document.getElementById("login-psw").value;
   if (!email || !psw) {
-    document.getElementById("formMessage").innerHTML =
+    document.getElementById("loginFormMessage").innerHTML =
       "Please enter email and password.";
     return false;
   }
   return true;
 }
 
-async function signIn() {
+function validateSignupInput() {
+	let email = document.getElementById("signup-email").value;
+	let psw = document.getElementById("signup-psw").value;
+	let pswConfirm = document.getElementById("confirm-psw").value;
+
+	if (!email || !psw || !pswConfirm) {
+	  document.getElementById("signupFormMessage").innerHTML =
+		"Please fill in all fields.";
+	  return false;
+	}
+	if (psw != pswConfirm) {
+	  document.getElementById("signupFormMessage").innerHTML =
+	    "Passwords do not match.";
+	  return false;
+	}
+	return true;
+  }
+
+async function login() {
   const body = {
-    email: document.getElementById("email").value,
-    psw: document.getElementById("psw").value,
+    email: document.getElementById("login-email").value,
+    psw: document.getElementById("login-psw").value,
   };
   try {
     let response = await axios({
       method: "POST",
-      url: "/signin",
+      url: "/login",
       data: body,
     });
     if (response.status == 200) {
@@ -44,7 +69,29 @@ async function signIn() {
   } catch (error) {
     console.log(error.response);
     if (error.response.data) {
-      document.getElementById("formMessage").innerHTML = error.response.data;
+      document.getElementById("loginFormMessage").innerHTML = error.response.data;
+    }
+  }
+}
+
+async function signup() {
+  const body = {
+    email: document.getElementById("signup-email").value,
+    psw: document.getElementById("signup-psw").value,
+  };
+  try {
+    let response = await axios({
+      method: "POST",
+      url: "/signup",
+      data: body,
+    });
+    if (response.status == 200) {
+      window.location.href = "/member";
+    }
+  } catch (error) {
+    console.log(error.response);
+    if (error.response.data) {
+      document.getElementById("signupFormMessage").innerHTML = error.response.data;
     }
   }
 }
