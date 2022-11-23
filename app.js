7a3c9bba55058c44b55e3072880aa9ae2c4333f3
jgrk28 <jacobkaplan28@gmail.com>
2022-11-23 11:07:17 +0300

    Connect mysql database for login

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql");
 const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "winstagram"
+});
+conn.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+})
 
 app.use(express.static("public"));
 
@@ -17,13 +30,25 @@ app.get("/member", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  if (req.body.email == "test" && req.body.password == "test") {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(401);
-  }
+  let email = req.body.email;
+  const sqlQuery = `SELECT password FROM users WHERE username = '${email}'`;
+  conn.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    let inputPassword = req.body.password;
+    if (result[0] && inputPassword == result[0].password) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(401);
+    }
+  })
+
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on ${port}`);
 });
+
+  // conn.end((err) => {
+  //   if (err) throw err;
+  //   console.log("Disconnected!");
+  // })
